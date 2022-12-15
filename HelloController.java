e72c1578fc9ea06da3017f8c2a99b6e7fc4f769a
AlexeyMorev <115208874+AlexeyMorev@users.noreply.github.com>
2022-12-15 12:53:49 +0200

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,16 @@
+package ua.mv.springcource;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello_world")
+    public String hello() {
+        return "hello_world";
+    }
+}
